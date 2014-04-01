26417a7e99eb8df82f2f580d52b76198970d0be6
Hongbin Zuo <hongbinzuo@163.com>
2014-04-01 21:10:24 +0800

    Initial commit

---
+++ src/com/freelemon/chapter1/Chapter1.java
@@ -0,0 +1,28 @@
+package com.freelemon.chapter1;
+
+/**
+ * Created by Hongbin on 2014/4/1.
+ */
+public class Chapter1 {
+    public static void exercise111() {
+        System.out.println("The exercise 1.1.1 ------");
+        int a = ( 0 + 15 ) /2;
+        double b = 2.0e-6 * 1000000000.1;
+        boolean c = true && false || true && true;
+
+        System.out.println("the value of a:" + a);
+        System.out.println("the value of b:" + b);
+        System.out.println("the value of c:" + c);
+
+    }
+
+    public static void exercise112(){
+        System.out.println("The exercise 1.1.2 ------");
+        double a = (1+2.236)/2;
+    }
+
+    public static void main(String[] args){
+        exercise111();
+
+    }
+}
