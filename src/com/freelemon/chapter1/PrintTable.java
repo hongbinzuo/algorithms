68e0cb61b4aa0b96ab8cb07b64c8dd52d3168c93
Hongbin Zuo <hongbinzuo@163.com>
2014-04-02 22:12:28 +0800

    4/2

---
+++ src/com/freelemon/chapter1/PrintTable.java
@@ -0,0 +1,12 @@
+package com.freelemon.chapter1;
+
+/**
+ * Created by Hongbin on 2014/4/2.
+ */
+public class PrintTable {
+    public static void main(String[] args){
+
+
+        //while (System.in.read()
+    }
+}
