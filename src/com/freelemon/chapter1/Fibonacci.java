184c1d714f0cf37934c441f3b2ab2613b25ce7ed
Hongbin Zuo <hongbinzuo@163.com>
2014-04-02 20:27:37 +0800

    4/2

--- src/com/freelemon/chapter1/Fibonacci.java
+++ src/com/freelemon/chapter1/Fibonacci.java
@@ -4,23 +4,44 @@ package com.freelemon.chapter1;
  * Created by Hongbin on 2014/4/2.
  */
 public class Fibonacci {
-    private static int[] fArray = null;
+    private static int length = 1000;
+    private static long[] fArray = null;
 
     public static long fOld(int N){
         if (N==0) return 0;
         if (N==1) return 1;
-        return fOld(N-1)+fOld(N-2);
+        return fOld(N - 1)+fOld(N - 2);
+    }
+
+    public static void setMaxLength(int len){
+        length = len;
     }
 
     public static long fNew(int N){
-        if (fArray == null) fArray = new int[N+1];
+        if (fArray == null) fArray = new long[length];
 
-        if(N==0) {
-            fArray[0] = 0;
-        } else if(N==1) {
-            fArray[1] = 1;
-        } else {
-            fArray[N] = fArray[N-1] + fArray[N-2];
+        System.out.println(fArray.length);
+
+        for (int i = 0; i < fArray.length; i++) {
+            if (i==0) {
+                fArray[i] = 0;
+            } else if (i==1) {
+                fArray[i] = 1;
+            } else  {
+                fArray[i] = -1;
+            }
+        }
+
+        int i = 0;
+        for (i = 0; i < fArray.length ; i++) {
+            if (fArray[i] != -1)
+                continue;
+            else
+                break;
+        }
+
+        for (int j = i; j < fArray.length ; j++) {
+            fArray[j] = fArray[j-2]+ fArray[j-1];
         }
 
         return fArray[N];
@@ -29,7 +50,7 @@ public class Fibonacci {
 
 
     public static void main(String[] args){
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println(i+ " " + fNew(i));
         }
     }
